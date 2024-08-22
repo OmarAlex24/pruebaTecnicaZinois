696174b18e0d6b404974581bcda16f9ba0b62db6
OmarAlex24 <artcate208@gmail.com>
2024-08-22 09:05:20 -0600

    feat: Add front-end files and configurations

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-9xl">Hola mundo</h1>
+      <Button>Click me</Button>
     </>
   );
 }
